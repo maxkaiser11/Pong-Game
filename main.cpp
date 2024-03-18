7d1c5e800ebaaeb2c4dbaa419d3795204bc1307e
Max <max@kaiser.family>
2024-03-18 12:35:19 +0100

    added paddle collisions

--- main.cpp
+++ main.cpp
@@ -24,6 +24,19 @@ public:
 };
 
 class Paddle {
+
+  protected:
+  void LimitMovement()
+  {
+    if (y <= 0)
+    {
+      y = 0;
+    }
+    if (y + height >= GetScreenHeight())
+    {
+      y = GetScreenHeight() - height;
+    }
+  }
 public:
   float x, y;
   float width, height;
@@ -39,14 +52,7 @@ public:
       y += speed;
     }
 
-    if (y <= 0)
-    {
-      y = 0;
-    }
-    if (y + height >= GetScreenHeight())
-    {
-      y = GetScreenHeight() - height;
-    }
+    LimitMovement();
   }
 };
 
@@ -64,6 +70,7 @@ class CpuPaddle : public Paddle
     {
       y += speed;
     }
+    LimitMovement();
   }
 };
 
@@ -105,6 +112,17 @@ int main() {
     player.Update();
     cpu.Update(ball.y);
 
+    // Checking for Collisions
+    if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{player.x, player.y, player.width, player.height}))
+    {
+      ball.speed_x *= -1;
+    }
+
+    if(CheckCollisionCircleRec(Vector2{ball.x, ball.y}, ball.radius, Rectangle{cpu.x, cpu.y, cpu.width, cpu.height}))
+    {
+      ball.speed_x *= -1;
+    }
+    
     // Drawing
     ClearBackground(BLACK);
     DrawLine(screen_width / 2, 0, screen_width / 2, screen_height, WHITE);
